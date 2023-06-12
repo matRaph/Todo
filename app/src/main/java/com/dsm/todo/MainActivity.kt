a67cbf36d03f2c73c0d6bef871d3c1139e3f041f
Raphael Matias <r.avelino@escolar.ifrn.edu.br>
2023-06-12 14:27:47 -0300

    Trabalhando na navegação

--- app/src/main/java/com/dsm/todo/MainActivity.kt
+++ app/src/main/java/com/dsm/todo/MainActivity.kt
@@ -2,6 +2,9 @@ package com.dsm.todo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -13,8 +16,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.dsm.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addTodoButton = findViewById(R.id.addTodoButton) as Button
+
+        //TODO: Declarar navController
+
+        // set on-click listener
+        addTodoButton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            //navController.navigate(R.id.action_mainActivity_to_addTodoActivity)
+        }
+    }
+}
+
+class AddTodoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.criar_todo)
     }
 }
